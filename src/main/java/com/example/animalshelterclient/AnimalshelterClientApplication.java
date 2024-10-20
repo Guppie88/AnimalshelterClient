ec952e6857aeb09143037212dc8543b5541d1c57
Andrea Sveinsdottir <121334896+Guppie88@users.noreply.github.com>
2024-10-20 11:01:18 +0200

    initial commit

---
+++ src/main/java/com/example/animalshelterclient/AnimalshelterClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalshelterclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalshelterClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalshelterClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/animalshelterclient/AnimalshelterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalshelterclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalshelterClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
